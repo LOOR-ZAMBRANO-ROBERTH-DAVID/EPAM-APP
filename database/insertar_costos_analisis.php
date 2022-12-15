adad7c8ef636f7cf4fa588fc35f4d8f8b360541c
D0oppa <devgarcia.004@gmail.com>
2022-12-15 09:00:17 -0500

    Nueva Rama

--- database/insertar_costos_analisis.php
+++ database/insertar_costos_analisis.php
@@ -8,6 +8,7 @@ $codigo = $_POST['codigo'];
 $costo = $_POST['costo'];
 $descripcion = $_POST['descripcion'];
 
+//hola
 //crear tablas
 $crear_tablas = "CREATE TABLE IF NOT EXISTS  $database.ep_costos_analisis(
     analisis VARCHAR(200),
